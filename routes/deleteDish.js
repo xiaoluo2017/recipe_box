2a27f5bf1ee54150ce56350950cbc15779eaab81
Xiao Luo <xiao2017luo@gmail.com>
2017-10-19 20:48:35 -0700

    Initial commit

---
+++ routes/deleteDish.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+router.post('/', function(req, res, next) {
+  mongo.connect("mongodb://xiaoluo:one123@ds125255.mlab.com:25255/recipe_box", function(err, db) {
+    if (err) throw err;
+    var collection = db.collection("dishes");
+    collection.remove({
+      _id: ObjectId(req.body._id)
+    }, function (err) {
+      if (err) throw err;
+      db.close();
+    })
+    res.send();
+  })
+});
+
+module.exports = router;
